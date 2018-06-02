301e4523a9660120027a5e8637ad25d8bcdd667e
naifengliu <naifengliu.us@gmail.com>
2018-06-02 11:36:16 -0400

    add_filter

--- save_personal_data.py
+++ save_personal_data.py
@@ -1,10 +1,13 @@
 import grouping
-
 names = []
 
 for key in grouping.matching.keys():
     names.append(key)
     names.append(grouping.matching[key][0])
 
-print names
-print len(names)
+with open("./data/combined_filtered", "w") as w:
+    with open("./data/combined") as f:
+        for line in f.readlines():
+            if int(float(line.split(",")[0])) in names:
+                w.write(line)
+
