db3e501cd42dd883cbc41ce241d1bc22ec4b5524
Godot <naifengliu.us@gmail.com>
2018-06-03 17:16:36 -0400

    init

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -1,7 +1,30 @@
 import numpy as np
 
-x = np.loadtxt("./result/cnn/t0")
 
-print x.shape
-print x[:, 0]
-print x[:, 0].shape
+def cal_per_result(method):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    for num in range(5):
+        x = np.loadtxt("./result/" + method + "/t" + str(num))
+        print x.shape
+        print x[:, 0]
+        print x[:, 0].shape
+
+        for i in range(len(x[:, 1])):
+            if i < 204:
+                if x[:, 1][i] >= 0.5:
+                    a += 1
+                else:
+                    b += 1
+            else:
+                if x[:, 1][i] >= 0.5:
+                    d += 1
+                else:
+                    c += 1
+    return a, b, d, c
+
+
+print cal_per_result("cnn")
+print cal_per_result("random_forest")
