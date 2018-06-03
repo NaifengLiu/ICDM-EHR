87129e0dec1d79e4033b28722b7ba70154ca91e7
Godot <naifengliu.us@gmail.com>
2018-06-03 17:19:28 -0400

    init

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def cal_per_result(method):
+def cal_per_result_cnn():
     a = 0
     b = 0
     c = 0
     d = 0
     for num in range(5):
-        x = np.loadtxt("./result/" + method + "/t" + str(num))
+        x = np.loadtxt("./result/cnn/t" + str(num))
         for i in range(len(x[:, 1])):
             if i < 204:
                 if x[:, 1][i] >= 0.5:
@@ -22,5 +22,26 @@ def cal_per_result(method):
     return a, b, d, c
 
 
-print cal_per_result("cnn")
-print cal_per_result("random_forest")
+def cal_per_result_rf(method):
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    for num in range(5):
+        x = np.loadtxt("./result/" + method + "/t" + str(num))
+        for i in range(len(x[:, 0])):
+            if i < 204:
+                if x[:, 0][i] >= 0.5:
+                    a += 1
+                else:
+                    b += 1
+            else:
+                if x[:, 0][i] >= 0.5:
+                    d += 1
+                else:
+                    c += 1
+    return a, b, d, c
+
+
+print cal_per_result_cnn()
+print cal_per_result_rf()
