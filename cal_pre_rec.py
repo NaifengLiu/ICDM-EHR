59a33b55ca061f1b88044fa0da9d14d4028a1a5b
Godot <naifengliu.us@gmail.com>
2018-06-04 13:05:11 -0400

    init

--- cal_pre_rec.py
+++ cal_pre_rec.py
@@ -1,13 +1,13 @@
 import numpy as np
 
 
-def cal_per_result_over_all(method):
+def cal_per_result_over_all(method, v_or_t):
     a = 0
     b = 0
     c = 0
     d = 0
     for num in range(5):
-        x = np.loadtxt("./result/" + method + "/t" + str(num))
+        x = np.loadtxt("./result/" + method + "/" + v_or_t + str(num))
         for i in range(len(x[:, 1])):
             if i < 204:
                 if x[:, 1][i] >= 0.5:
@@ -23,6 +23,33 @@ def cal_per_result_over_all(method):
     print "TP: ", a, "FP: ", b, "FN: ", d, "TN: ", c
 
 
-cal_per_result_over_all("cnn")
-cal_per_result_over_all("random_forest")
-cal_per_result_over_all("lr")
+# cal_per_result_over_all("cnn", "v")
+# cal_per_result_over_all("cnn", "t")
+# cal_per_result_over_all("random_forest", "v")
+# cal_per_result_over_all("random_forest", "t")
+# cal_per_result_over_all("lr", "v")
+# cal_per_result_over_all("lr", "t")
+
+
+q = np.array([0.9, 0.7, 0.1, 0.8, 0.5])
+
+print np.argsort(-q)
+
+
+def cal_pre_rec(method, v_or_t):
+    for num in range(5):
+        precision = 0
+        recall = 0
+        tag = []
+        x = np.loadtxt("./result/" + method + "/" + v_or_t + str(num))
+        tmp = np.argsort(-x[:, 1])
+        for i in range(len(tmp)):
+            if tmp[i] < 204:
+                precision += 1
+            if precision >= 204/20*(len(tag) + 1):
+                tag.append(float(precision) / float(i))
+        print tag
+
+
+cal_pre_rec("cnn", "v")
+
